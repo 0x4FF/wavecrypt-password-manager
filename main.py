888b634c4fb8e596a50c20a5a1b6f642ad30c863
1waves <112463082+1waves@users.noreply.github.com>
2022-09-09 23:50:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
+from distutils.log import error
 import os, requests, time;from datetime import date; import colorama; from colorama import Fore; from cryptography.fernet import Fernet
 
-directory = "Directory-To-Save-Passwords"
+directory = "C:\\Users\\crese\\Downloads\\Important\\Coding\\Coding\\python\\PW manager\\creds\\"
 today = date.today()
 key = Fernet.generate_key()
 
-with open('config/key.key', 'wb') as kf:
-    kf.write(key)
-with open('config/key.key', 'rb') as fk:
-    key = fk.read()
+
 
 if os.name == 'nt':
     cmd = "cls"
 elif os.name == 'posix':
     cmd = "clear"
-    
+
 def main_menu():
-    option = input("[1] Add Login   [2] View Login  [3] Lock Files  [4] Unlock Files: ")
+    option = input("[1] Add Login   [2] View Login  [3] Lock Files  [4] Unlock Files   [5] Load Key: ")
     if option == "1":
         os.system(cmd)
         add_login()
@@ -28,6 +26,9 @@ def main_menu():
     elif option == "4":
         os.system(cmd)
         unlock_files()
+    elif option == "5":
+        os.system(cmd)
+        load_key()
 
 def add_login():
     site = input("Enter Site Domain: ")
@@ -63,6 +64,7 @@ def view_login():
     for file in os.listdir(directory):
         file_names.append(file)
         count+=1
+        
         print(f"File #{count}:  {file}")
 
     choice = input("Which file?: ")
@@ -91,19 +93,31 @@ def lock_files():
         print(f"{Fore.GREEN}[+]Files locked succesfully{Fore.RESET}")
         time.sleep(3)
         os.system(cmd)
-        
+        main_menu()
+       
 
 def unlock_files():
     fk = Fernet(key)
     for file in os.scandir(directory):
-        with open(file, 'rb') as file_c:
-            e_data = file_c.read()
-            d_data = fk.decrypt(e_data)
-        with open(file, 'wb') as file_e:
-            file_e.write(d_data)
-        print(f"{Fore.GREEN}[+]Files Unlocked succesfully{Fore.RESET}")
+        try:    
+
+            with open(file, 'rb') as file_c:
+                e_data = file_c.read()
+                d_data = fk.decrypt(e_data)
+            with open(file, 'wb') as file_e:
+                file_e.write(d_data)
+                print(f"{Fore.GREEN}[+]Files Unlocked succesfully{Fore.RESET}")
+        except:
+            print("Couldnt unlock file, unknown error")
         time.sleep(3)
         os.system(cmd)
-       
+        main_menu()
 
+def load_key():
+    with open('config/key.key', 'wb+') as kf:
+        kf.write(key)
+
+    print("done")
+    
+        
 main_menu()
